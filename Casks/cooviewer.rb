fde0609e10a6594566fa36332ebc3574da5f0c59
kni <kni927@gmail.com>
2026-07-10 15:23:55 +0900

    notrazied version release

--- Casks/cooviewer.rb
+++ Casks/cooviewer.rb
@@ -1,6 +1,6 @@
 cask "cooviewer" do
-  version "1.3.4"
-  sha256 "d23b8bcffcfc0d1274951daa7fe5de86f5d3807638260189b0cc159cddd93af1"
+  version "1.3.6"
+  sha256 "6b74d70f683f5bf67130792ac5332db90678d8f2ecb574d6d37c714f08a20973"
 
   url "https://github.com/kni927/cooViewer/releases/download/v#{version}/cooViewer.app.zip"
   name "cooViewer"
