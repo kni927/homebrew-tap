05f828432d27354716a3fc9e2d399e90561043ef
kni <kni927@gmail.com>
2026-07-11 16:55:57 +0900

    fix typo

--- Casks/cooviewer.rb
+++ Casks/cooviewer.rb
@@ -2,7 +2,7 @@ cask "cooviewer" do
   version "1.4.0"
   sha256 "6e5480caee961010d8b32f72ddaf5c49e839209fbbf6a3b32c10897032e99e50"
 
-  url "https://github.com/kni927/cooViewer/releases/download/v#{version}/cooViewer.app.zip"
+  url "https://github.com/kni927/cooViewer/releases/download/v#{version}/cooViewer-#{version}.app.zip"
   name "cooViewer"
   desc "Simple comic viewer for macOS"
   homepage "https://github.com/kni927/cooViewer"
