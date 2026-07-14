a3871ca9340c59f2a500a5a352bac42265d39c0d
kni <kni927@gmail.com>
2026-07-15 00:53:35 +0900

    cooviewer 1.5.0
    Native QuickLook extensions for cbz/cbr, fixed Get Info version string.

--- Casks/cooviewer.rb
+++ Casks/cooviewer.rb
@@ -1,6 +1,6 @@
 cask "cooviewer" do
-  version "1.4.0"
-  sha256 "6e5480caee961010d8b32f72ddaf5c49e839209fbbf6a3b32c10897032e99e50"
+  version "1.5.0"
+  sha256 "65d17d513a2276e3f09fef52bdf49d676edc77ed36fd1416c207b7970704ddda"
 
   url "https://github.com/kni927/cooViewer/releases/download/v#{version}/cooViewer-v#{version}.zip"
   name "cooViewer"
